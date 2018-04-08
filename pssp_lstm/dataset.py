d0b261a99e453b1301092dc5bfcf315fe5a0a5a7
Dillon Daudert <dillongdaudert@gmail.com>
2018-04-07 20:23:22 -0400

    Move pssp_lstm model to correct folder

---
+++ pssp_lstm/dataset.py
@@ -0,0 +1,78 @@
+"""Create a tf.data.Dataset input pipeline."""
+
+from pathlib import Path
+import tensorflow as tf, numpy as np
+
+def create_dataset(hparams, mode):
+    """
+    Create a tf.Dataset from a file.
+    Args:
+        hparams - Hyperparameters for the dataset
+        mode    - the mode, one of tf.contrib.learn.ModeKeys.{TRAIN, EVAL, INFER}
+    Returns:
+        dataset - A tf.data.Dataset object
+    """
+
+    if mode == tf.contrib.learn.ModeKeys.TRAIN:
+        input_file = hparams.train_file
+        shuffle = True
+        batch_size = hparams.batch_size
+        num_epochs = hparams.num_epochs
+    elif mode == tf.contrib.learn.ModeKeys.EVAL:
+        input_file = hparams.valid_file
+        shuffle = False
+        batch_size = hparams.batch_size
+        num_epochs = 1
+    else:
+        print("INFER mode not supported.")
+        quit()
+
+    parser = cpdb_parser
+
+    dataset = tf.data.TFRecordDataset(input_file)
+
+    # perform the appropriate transformations and return
+    dataset = dataset.cache()
+
+    if shuffle:
+        dataset = dataset.shuffle(buffer_size=batch_size*100)
+
+    dataset = dataset.repeat(num_epochs)
+
+    # parse the records
+    dataset = dataset.map(lambda x:parser(x, hparams), num_parallel_calls=4)
+
+
+    # padded batch to support sequences of multiple lengths
+    dataset = dataset.padded_batch(
+            batch_size,
+            padded_shapes=(tf.TensorShape([None, hparams.num_features]),
+                           tf.TensorShape([None, hparams.num_labels]),
+                           tf.TensorShape([])))
+
+    # prefetch on CPU
+    dataset = dataset.prefetch(1)
+
+    return dataset
+
+def cpdb_parser(record, hparams):
+    """
+    Parse a CPDB tfrecord Record into a tuple of tensors.
+    """
+
+    keys_to_features = {
+        "seq_len": tf.FixedLenFeature([], tf.int64),
+        "seq_data": tf.VarLenFeature(tf.float32),
+        "label_data": tf.VarLenFeature(tf.float32),
+        }
+
+    parsed = tf.parse_single_example(record, keys_to_features)
+
+    seq_len = parsed["seq_len"]
+    seq_len = tf.cast(seq_len, tf.int32)
+    seq = tf.sparse_tensor_to_dense(parsed["seq_data"])
+    label = tf.sparse_tensor_to_dense(parsed["label_data"])
+    seq = tf.reshape(seq, [-1, hparams.num_features])
+    tgt_outputs = tf.reshape(label, [-1, hparams.num_labels])
+
+    return seq, tgt_outputs, seq_len
---
+++ pssp_lstm/pssp_lstm.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+"""Driver for training and running models."""
+import argparse as ap
+from pathlib import Path
+from train import train
+from evaluate import evaluate
+from hparams import HPARAMS
+
+def main():
+
+
+    # Define the main argument parser
+    parser = ap.ArgumentParser(prog="pssp_lstm", description="Train and run models.",
+                               argument_default=ap.SUPPRESS)
+
+    subparsers = parser.add_subparsers(title='subcommands')
+
+    # -- training subparser --
+    tr_parser = subparsers.add_parser("train", help="Train a model")
+
+    tr_parser.add_argument("datadir", type=str,
+                           help="the directory where the .tfrecords data is located")
+
+    tr_parser.add_argument("logdir", type=str,
+                           help="the directory where model checkpoints and logs will\
+                                 be saved")
+    tr_parser.add_argument("-l", "--logging", action="store_true",
+                           help="toggle to enable tf.summary logs (disabled by default)")
+
+    tr_parser.set_defaults(entry="train")
+
+    ev_parser = subparsers.add_parser("evaluate", help="Evaluate a trained model")
+
+    ev_parser.add_argument("datadir", type=str,
+                           help="the directory where the cpdb_513.tfrecords file is located.")
+    ev_parser.add_argument("ckpt", type=str,
+                           help="a checkpoint file for a trained model. This will\
+                                 look something like this:\
+                                 /path/ckpt/ckpt-4100")
+    ev_parser.set_defaults(entry="evaluate")
+
+    args = parser.parse_args()
+
+
+    if args.entry == "train":
+        # run training
+        HPARAMS.logging = args.logging
+
+        logpath = Path(args.logdir)
+        HPARAMS.logdir = str(logpath.absolute())
+        HPARAMS.train_file = str(Path(args.datadir, "cpdb_train.tfrecords").absolute())
+        HPARAMS.valid_file = str(Path(args.datadir, "cpdb_valid.tfrecords").absolute())
+
+        train(HPARAMS)
+
+    elif args.entry == "evaluate":
+        # evaluate a trained network on the test data
+        HPARAMS.valid_file = str(Path(args.datadir, "cpdb_513.tfrecords").absolute())
+        HPARAMS.model_ckpt = str(Path(args.ckpt).absolute())
+
+        evaluate(HPARAMS)
+
+
+
+if __name__ == "__main__":
+    main()
