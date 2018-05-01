00fc688ffd942ebbc7f843290d2331e77c5fb275
Dillon Daudert <dillongdaudert@gmail.com>
2018-05-01 16:45:08 -0400

    Update hparameters to match large model; smaller batches

--- pssp_lstm/hparams.py
+++ pssp_lstm/hparams.py
@@ -5,12 +5,12 @@ import tensorflow as tf
 HPARAMS = tf.contrib.training.HParams(
     num_features=43,
     num_labels=9,
-    num_units=300,
+    num_units=500,
     num_layers=3,
-    num_dense_units=200,
+    num_dense_units=400,
     dropout=0.5,
-    batch_size=128,
-    num_epochs=125,
+    batch_size=64,
+    num_epochs=100,
     max_gradient_norm=0.5,
-    num_keep_ckpts=4,
+    num_keep_ckpts=5,
 )
