17604f39a578eee6232fb4c56998a52b8042108c
Dillon Daudert <dillongdaudert@gmail.com>
2018-05-13 19:19:51 -0400

    Add basic lm_pretrain

---
+++ lm_pretrain/hparams.py
@@ -0,0 +1,16 @@
+"""Hyperparameters for PSSP LSTM"""
+
+import tensorflow as tf
+
+HPARAMS = tf.contrib.training.HParams(
+    num_features=43,
+    num_labels=22,
+    num_units=300,
+    num_layers=3,
+    num_dense_units=200,
+    dropout=0.5,
+    batch_size=50,
+    num_epochs=250,
+    max_gradient_norm=0.5,
+    num_keep_ckpts=6,
+)
