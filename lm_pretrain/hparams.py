210894b6e13c208b40d993ba74155d7d82ff510c
Dillon Daudert <dillongdaudert@gmail.com>
2018-05-19 22:48:55 -0400

    Split hparams into lm and bdrnn hparams

--- lm_pretrain/hparams.py
+++ lm_pretrain/hparams.py
@@ -2,19 +2,33 @@
 
 import tensorflow as tf
 
-HPARAMS = tf.contrib.training.HParams(
-    num_features=43,
-    num_inp_labels=22,
-    num_tgt_labels=9,
-    num_units=500,
-    num_layers=1,
-    num_dense_units=200,
-    dropout=0.5,
-    batch_size=50,
-    num_epochs=10,
-    max_gradient_norm=0.5,
-    num_keep_ckpts=6,
-    model="bdrnn",
-    pretrained=True,
-    lm_ckpt="/home/dillon/models/lm_pretrain/lm_model/ckpts/ckpt-1000"
-)
+hparams = {
+   "bdrnn": tf.contrib.training.HParams(
+            num_features=43,
+            num_inp_labels=22,
+            num_tgt_labels=9,
+            num_units=500,
+            num_layers=2,
+            input_proj_size=50,
+            num_dense_units=200,
+            dropout=0.5,
+            batch_size=50,
+            num_epochs=10,
+            max_gradient_norm=0.5,
+            num_keep_ckpts=6,
+            model="bdrnn"),
+   "lm": tf.contrib.training.HParams(
+            num_features=43,
+            num_inp_labels=22,
+            num_tgt_labels=9,
+            num_units=500,
+            num_layers=0,
+            input_proj_size=50,
+            num_dense_units=200,
+            dropout=0.5,
+            batch_size=50,
+            num_epochs=10,
+            max_gradient_norm=0.5,
+            num_keep_ckpts=6,
+            model="lm")
+   }
