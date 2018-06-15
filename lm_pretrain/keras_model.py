81f91a051797f85d0d2266861aaaabbf74117ac2
Dillon Daudert <dillongdaudert@gmail.com>
2018-06-14 21:30:14 -0400

    Create tables; consolidate to one model. Memory issues still

--- lm_pretrain/keras_model.py
+++ lm_pretrain/keras_model.py
@@ -5,10 +5,10 @@ import tensorflow.keras as keras
 from tensorflow.keras.layers import Input, LSTMCell, RNN, Dense, concatenate, Masking
 from tensorflow.keras.models import Model
 from .dataset import create_dataset
+from .lookup import create_lookup_table
 
-def bdrnn_model(hparams):
+def create_models(hparams):
 
-    # create a bidirectional language model
     seq_id = Input(shape=tf.TensorShape([]))
     seq_lens = Input(shape=tf.TensorShape([]))
     seq_in = Input(shape=(None, 23))
@@ -25,8 +25,8 @@ def bdrnn_model(hparams):
     embed = Dense(units=10, use_bias=False, name="embed")(seq_in_mask)
     lm_x = concatenate([embed, phyche_in_mask])
 
-    fw_cells = [LSTMCell(units=hparams.lm_num_units) for i in range(hparams.lm_num_layers)]
-    bw_cells = [LSTMCell(units=hparams.lm_num_units) for i in range(hparams.lm_num_layers)]
+    fw_cells = [LSTMCell(units=hparams.lm_num_units) for _ in range(hparams.lm_num_layers)]
+    bw_cells = [LSTMCell(units=hparams.lm_num_units) for _ in range(hparams.lm_num_layers)]
 
     lm_fw_out = RNN(fw_cells, return_sequences=True, name="lm_fw")(lm_x)
     lm_bw_out = RNN(bw_cells, return_sequences=True, name="lm_bw", go_backwards=True)(lm_x)
@@ -41,13 +41,13 @@ def bdrnn_model(hparams):
     # --------
 
     # create inputs to bdrnn
-    x = concatenate([embed, phyche_in_mask, pssm_in_mask, lm_dense2])
+    x = concatenate([embed, pssm_in_mask, lm_dense2])
 
     fw_cells = [LSTMCell(units=hparams.num_units) for _ in range(hparams.num_layers)]
     bw_cells = [LSTMCell(units=hparams.num_units) for _ in range(hparams.num_layers)]
 
-    fw_out = RNN(fw_cells, return_sequences=True)(x)
-    bw_out = RNN(bw_cells, return_sequences=True, go_backwards=True)(x)
+    fw_out = RNN(fw_cells, return_sequences=True, name="bdrnn_fw")(x)
+    bw_out = RNN(bw_cells, return_sequences=True, name="bdrnn_bw", go_backwards=True)(x)
     rnn_out = concatenate([fw_out, bw_out])
 
     dense1 = Dense(units=100, activation="relu")(rnn_out)
@@ -55,8 +55,11 @@ def bdrnn_model(hparams):
 
     out = Dense(units=10, activation="softmax")(dense2)
 
-    return Model(inputs=[seq_id, seq_lens, seq_in, phyche_in, pssm_in],
-                 outputs=[out])
+    bdrnn_model = Model(inputs=[seq_id, seq_lens, seq_in, phyche_in, pssm_in],
+                        outputs=[out])
+    lm_model = Model(inputs=[seq_id, seq_lens, seq_in, phyche_in, pssm_in],
+                     outputs=[lm_out])
+    return {"bdrnn": bdrnn_model, "lm": lm_model}
 
 
 
@@ -66,22 +69,27 @@ if __name__ == "__main__":
         train_file="/home/dillon/data/cpdb2/cpdb_train.tfrecords",
         valid_file="/home/dillon/data/cpdb2/cpdb_valid.tfrecords",
         test_file="/home/dillon/data/cpdb2/cpdb513_test.tfrecords",
-        num_layers=2,
-        num_units=300,
-        batch_size=64,
+        num_layers=1,
+        num_units=200,
+        batch_size=50,
         num_epochs=10,
         steps_per_epoch=100,
         validation_steps=10,
+        lm_num_layers=2,
+        lm_num_units=300,
         model="bdrnn",
         )
     lm_hparams = tf.contrib.training.HParams(
-        num_phyche_features=7,
-        num_layers=1,
-        num_units=400,
         )
     # set session
     sess = tf.Session()
     tf.keras.backend.set_session(sess)
+
+    # create lookup tables
+    hparams.prot_lookup_table = create_lookup_table("prot")
+    hparams.prot_reverse_lookup_table = create_lookup_table("prot", reverse=True)
+    hparams.struct_lookup_table = create_lookup_table("struct")
+    hparams.struct_reverse_lookup_table = create_lookup_table("struct", reverse=True)
 
     train_dataset = create_dataset(hparams, tf.contrib.learn.ModeKeys.TRAIN)
     valid_dataset = create_dataset(hparams, tf.contrib.learn.ModeKeys.EVAL)
@@ -90,7 +98,8 @@ if __name__ == "__main__":
     # initialize tables
     sess.run([tf.tables_initializer()])
 
-    model = bdrnn_model(hparams, lm_hparams)
+    models = create_models(hparams)
+    model = models["bdrnn"]
     model.summary()
 
     model.compile(optimizer="rmsprop",
@@ -101,7 +110,8 @@ if __name__ == "__main__":
               epochs=hparams.num_epochs,
               steps_per_epoch=hparams.steps_per_epoch,
               validation_data=valid_dataset,
-              validation_steps=hparams.validation_steps)
+              validation_steps=hparams.validation_steps,
+              callbacks=[tf.keras.callbacks.TensorBoard(log_dir="./logs/6")])
 
     print(model.predict(x=test_dataset, steps=1))
 
