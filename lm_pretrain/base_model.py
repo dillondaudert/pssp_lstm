cc6a521c4704a129553ee5634a9d1ef4e15c75a8
Dillon Daudert <dillongdaudert@gmail.com>
2018-09-07 13:13:42 -0400

    Do not handle cnn_bdlm outputs in base_model

--- lm_pretrain/base_model.py
+++ lm_pretrain/base_model.py
@@ -23,7 +23,6 @@ class BaseModel(object):
         tf.get_variable_scope().set_initializer(initializer)
 
         self.inputs = self.iterator.get_next()
-        self.outputs = None
         res = self._build_graph(hparams, self.inputs, mode, scope=scope)
 
         # Graph losses
@@ -36,8 +35,6 @@ class BaseModel(object):
             self.accuracy = res[2][0]
             self.confusion = res[2][1]
             self.update_metrics = res[3]
-            if len(res) == 5:
-                self.outputs = res[4]
 
         params = tf.trainable_variables()
 
@@ -129,7 +126,5 @@ class BaseModel(object):
                    self.confusion,
                    self.eval_summary,
                    self.update_metrics]
-        if self.outputs is not None:
-            fetches.append(self.outputs)
 
         return sess.run(fetches)
