6b8fad2e7d37cd733871351de54272250ea01d1b
Dillon Daudert <dillongdaudert@gmail.com>
2018-09-08 21:10:01 -0400

    Save the input/outputs of a ckpt as a dataframe in evaluate.py

--- lm_pretrain/evaluate.py
+++ lm_pretrain/evaluate.py
@@ -6,28 +6,52 @@ import tensorflow as tf, numpy as np
 import pandas as pd
 from .model_helper import create_model
 
-def evaluate(hparams, outfile=None):
+def evaluate(hparams, files, outfile=None):
     """Evaluate a trained model"""
 
-    eval_tuple = create_model(hparams, tf.contrib.learn.ModeKeys.EVAL)
+    cols = ["file", "id", "seq", "phyche", "pssm", "logits", "ss"]
+    hcols = ["h_0", "h_1", "h_2"]
+    recs = []
 
-    with eval_tuple.graph.as_default():
-        local_initializer = tf.local_variables_initializer()
+    for f in files:
+        hparams.valid_file = f
+        eval_tuple = create_model(hparams, tf.contrib.learn.ModeKeys.EVAL)
+        with eval_tuple.graph.as_default():
+            local_initializer = tf.local_variables_initializer()
 
-    print("Evaluating model on %s" % (hparams.valid_file))
+        print("Evaluating model on %s" % (hparams.valid_file))
+        # do evaluation
+        eval_tuple.model.saver.restore(eval_tuple.session, hparams.ckpt)
+        eval_tuple.session.run([eval_tuple.iterator.initializer, local_initializer])
+        while True:
+            try:
+                fetched = eval_tuple.model.eval(eval_tuple.session)
 
-    # do evaluation
-    eval_tuple.model.saver.restore(eval_tuple.session, hparams.ckpt)
-    eval_tuple.session.run([eval_tuple.iterator.initializer, local_initializer])
-    while True:
-        try:
-            x, probs, eval_loss, eval_acc, eval_cm, eval_summary, _ = eval_tuple.model.eval(eval_tuple.session)
-            # summary_writer.add_summary(summary, global_step)
-        except tf.errors.OutOfRangeError:
-            print("Eval Loss: %f, Eval Accuracy: %f" % (eval_loss,
-                                                        eval_acc))
-            print("Confusion Matrix (true label, predicted label):" )
-            print(eval_cm)
-            #df = pd.DataFrame(eval_cm)
-            #df.to_csv("./conf_matr.csv", index=False)
-            break
+                rec = (f,
+                       fetched["inputs"].id,
+                       fetched["inputs"].seq,
+                       fetched["inputs"].phyche,
+                       fetched["inputs"].pssm,
+                       fetched["logits"],
+                       fetched["inputs"].ss
+                       )
+                if "outputs" in fetched.keys():
+                    rec = rec + (fetched["outputs"].h_0,
+                                 fetched["outputs"].h_1,
+                                 fetched["outputs"].h_2,
+                                 )
+
+                recs.append(rec)
+
+                # summary_writer.add_summary(summary, global_step)
+            except tf.errors.OutOfRangeError:
+                break
+
+    df_cols = cols if len(cols) == len(recs[0]) else cols+hcols
+
+    df = pd.DataFrame.from_records(data=recs, columns=df_cols)
+
+    print(df.iloc[0])
+
+    if outfile is not None:
+        df.to_csv(outfile, index=False)
