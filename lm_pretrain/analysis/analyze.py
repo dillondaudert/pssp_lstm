15f3edaf4c015b8930fc1353fcb89e2060c2d491
Dillon Daudert <dillongdaudert@gmail.com>
2018-09-12 14:45:17 -0400

    Analyze calculates acc/class and /bin for list of files

--- lm_pretrain/analysis/analyze.py
+++ lm_pretrain/analysis/analyze.py
@@ -1,14 +1,24 @@
 import pandas as pd
 import numpy as np
-from .metrics import accuracy
+from .metrics import accuracy, accuracy_vs_len
 from .data_loader import load_data
 from ..lookup import STRUCT_ALPHABET
 
-def analyze(datafile: str, outfile: str):
-    print("Loading data from {datafile}")
-    df = load_data(datafile)
+def analyze(datafiles: list, outfile: str):
+    res_dfs = []
+    for f in datafiles:
+        print("Loading data from %s" % f)
+        df = load_data(f)
 
-    y_pred = df.logits.apply(lambda x: np.argmax(x, axis=-1))
-    y_true = df.ss.apply(lambda x: np.argmax(x, axis=-1))
+        y_pred = df.logits.apply(lambda x: np.argmax(x, axis=-1))
+        y_true = df.ss.apply(lambda x: np.argmax(x, axis=-1))
 
-    print(accuracy(y_pred, y_true, classes=STRUCT_ALPHABET))
+        acc_df = accuracy(y_true, y_pred, classes=STRUCT_ALPHABET)
+        acc_len_df = accuracy_vs_len(y_true, y_pred, df.len, boundaries=[0, 50, 100, 150, 200, 250, 300, 350, 400, 450, 500, 550, 600, 650, 700])
+
+        res_df = pd.concat([acc_df, acc_len_df])
+        res_df.columns = [df.dataset.iloc[0]+"_accuracy", df.dataset.iloc[0]+"_count"]
+        res_dfs.append(res_df)
+
+    res_df = pd.concat(res_dfs, axis=1)
+    print(res_df)
