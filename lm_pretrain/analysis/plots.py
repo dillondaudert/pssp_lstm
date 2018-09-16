86c053c601c7adbc76f18fdf39224c11fe5335b3
Dillon Daudert <dillongdaudert@gmail.com>
2018-09-16 01:12:42 -0400

    Add plotly line plot

--- lm_pretrain/analysis/plots.py
+++ lm_pretrain/analysis/plots.py
@@ -1,5 +1,6 @@
 from matplotlib import pyplot as plt
 import matplotlib
+import plotly.graph_objs as go
 
 def _plot_line_CI(ax,
                   x,
@@ -36,21 +37,21 @@ def line_CI_plot(xs,
     matplotlib.rc("figure", figsize=(14, 7))
     # font size to 14
     matplotlib.rc("font", size=14)
-    # remove top and right frame lines
-    #matplotlib.rc("axes.spines", top=False, right=False)
-    # remove grid lines
-    matplotlib.rc("axes", grid=False)
-    # set background color
-    matplotlib.rc("axes", facecolor="white")
     
     # create plot object
-    _, ax = plt.subplots()
+    fig, ax = plt.subplots()
     
-    for i in range(len(xs)):
-        _plot_line_CI(ax, xs[i], ys[i], sorted_xs[i], low_CIs[i], high_CIs[i], colors[i], labels[i])
+    ax.set_yscale("log", nonposy="clip")
+    ax.grid(True)
+    ax.yaxis.set_major_locator(plt.MaxNLocator(4))
+    ax.xaxis.set_major_locator(plt.MultipleLocator(5000))
+    ax.xaxis.set_minor_locator(plt.MultipleLocator(2500))
     
     # plot data, set linewidth, color, transparency,
     # provide label for the legend
+    for i in range(len(xs)):
+        _plot_line_CI(ax, xs[i], ys[i], sorted_xs[i], low_CIs[i], high_CIs[i], colors[i], labels[i])
+    
     # label axes and provide title
     ax.set_title(title)
     ax.set_xlabel(x_label)
@@ -58,4 +59,51 @@ def line_CI_plot(xs,
     
     # display legend
     ax.legend(loc="best")
+    fig
+    
+    
+def line_CI_plot_2(ys, 
+                   low_CIs, 
+                   high_CIs, 
+                   labels,
+                   colors):
+    
+    data = []
+    for i in range(len(labels)):
+        # add scatter plots for lower, mean, upper bounds
+        # for each series
+        lower_bound = go.Scatter(
+            name=labels[i]+" low CI",
+            x=ys[i].index,
+            y=low_CIs[i],
+            mode="lines",
+            marker=dict(color=colors[i], opacity=.4),
+            line=dict(width=0),
+            showlegend=False,)
+        trace = go.Scatter(
+            name=labels[i],
+            x=ys[i].index,
+            y=ys[i],
+            mode="lines",
+            line=dict(color=colors[i]),
+            fill="tonexty")
+        upper_bound = go.Scatter(
+            name=labels[i]+" high CI",
+            x=ys[i].index,
+            y=high_CIs[i],
+            mode="lines",
+            marker=dict(color=colors[i]),
+            line=dict(width=0),
+            fill="tonexty",
+            showlegend=False)
+
+        data += [lower_bound, trace, upper_bound]
+
+    layout = go.Layout(
+        yaxis=dict(title="Cross Entropy", type="log", autorange=True),
+        title="Validation Loss",
+        showlegend=True)
+    fig = go.FigureWidget(data=data, layout=layout)
+    return fig
+    
     
